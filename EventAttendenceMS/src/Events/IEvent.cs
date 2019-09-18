2bbfd9fe63daefe8f38ab625cc6e9be6b75616f2
tavisca-sgogawale <sgogawale@tavisca.com>
2019-09-18 15:26:03 +0530

    Added Interface contracts

--- EventAttendenceMS/src/Events/IEvent.cs
+++ EventAttendenceMS/src/Events/IEvent.cs
@@ -1,11 +1,16 @@
-﻿using System;
+﻿using EventAttendenceMS.src.Attendees;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace EventAttendenceMS.src.Events
 {
-    public class IEvent
+    public interface IEvent
     {
+        Guid Id { get; set; }
+        string Name { get; set; }
+        List<IAttendee> AttendeeList { get; set; } 
+        TimeSlot TimeSlot { get; set; }
     }
 }
---
+++ EventAttendenceMS/src/TimeSlot.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EventAttendenceMS.src
+{
+    public class TimeSlot
+    {
+        public TimeSlot(DateTime start, DateTime end)
+        {
+            if ( end < start )
+            {
+                throw new Exception("Start Date can not be after end date.");
+            }
+
+            Start = start;
+            End = end;
+        }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+
+
+    }
+}
