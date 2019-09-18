030f2bd3ab1aa230f85cd47b0482d353f538202c
tavisca-sgogawale <sgogawale@tavisca.com>
2019-09-18 14:40:23 +0530

    Updated IAttendee.

--- EventAttendenceMS/src/Attendees/IAttendee.cs
+++ EventAttendenceMS/src/Attendees/IAttendee.cs
@@ -1,12 +1,17 @@
-﻿using System;
+﻿using EventAttendenceMS.src.Events;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace EventAttendenceMS.src.Attendees
 {
-    interface IAttendee
+    public interface IAttendee
     {
+        string Name { get; set; }
+        Guid Id { get; set; }
+
+        void MarkAttendance(IEvent e);
 
     }
 }
